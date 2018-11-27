baf8e76f43931f73e3dedd3a7ca2045b9920fa09
Francisco Javier Ramos Carretero <francisco.javier.ramos.carretero@ericsson.com>
2018-11-27 18:25:41 +0100

    h2load

--- scripts/deletepodeecreate.sh
+++ scripts/deletepodeecreate.sh
@@ -3,20 +3,20 @@ declare -i var=0
 echo "Pods deleted:"$var
 while true
 do
-kubectl get pods -oname | grep eecreate | head -n 1 | xargs kubectl delete
+kubectl get pods -oname | grep eecreate | head -n 1 | xargs kubectl delete --grace-period=0 --force 
 var=$var+1
 echo "Pods deleted:"$var
-sleep 1
-kubectl get pods -oname | grep eecreate | head -n 2 | tail -n 1 | xargs kubectl delete
+sleep 15
+kubectl get pods -oname | grep eecreate | head -n 2 | tail -n 1 | xargs kubectl delete --grace-period=0 --force
 var=$var+1
 echo "Pods deleted:"$var
-sleep 1
-kubectl get pods -oname | grep eecreate | head -n 3 | tail -n 1 | xargs kubectl delete
+sleep 15
+kubectl get pods -oname | grep eecreate | head -n 3 | tail -n 1 | xargs kubectl delete --grace-period=0 --force
 var=$var+1
 echo "Pods deleted:"$var
-sleep 1
-kubectl get pods -oname | grep eecreate | head -n 4 | tail -n 1 | xargs kubectl delete
+sleep 15
+kubectl get pods -oname | grep eecreate | head -n 4 | tail -n 1 | xargs kubectl delete --grace-period=0 --force
 var=$var+1
 echo "Pods deleted:"$var
-sleep 1
+sleep 15
 done
